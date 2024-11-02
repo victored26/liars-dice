dd9571dd3b321e8536ea01c5b96c6c5a3b6672a1
Victor <vfernandez.fw@gmail.com>
2024-11-02 08:35:27 -0500

    Changes, and NPC class file

---
+++ npc.js
@@ -0,0 +1,84 @@
+import Player from './player.js'
+import { binCondCumProb } from './probability.js'
+
+class NPC extends Player {
+    constructor(game, name="Player") {
+        super(game, name);
+        this.p = 1 / this.settings.faces;
+    }
+    newTurn() {
+        // Rolls NPC's dice and records results in NPC's guesses
+        this.roll_dice();
+        this.guesses = {...this.rolled};
+    }
+    evaluateBid(lastBid) {
+        /* Returns false if NPC thinks the bid should be challenged. 
+        Otherwise, the NPC updates their guesses and returns true */
+        let number = lastBid['number'], face = lastBid['face'];
+
+        // Compare bid to player's board guesses
+        let guess_number = this.guesses[face]
+        if (number <= guess_number) {
+            return true;
+        }
+
+        // Determine conditional probability given NPC assessment
+        let condProb = binCondCumProb(totalDice, number, guess_number, this.p);
+        condProb += this.settings.bidEvalOffset;
+
+        // Roll a number from 0 to 1 and compare to conditional probability
+        if (Math.random() > condProb) {
+            return false;
+        }
+
+        // Modify NPC guesses to reflect bid being considered plausible
+        let addNumber = Math.round(condProb * (number - guess_number));
+        this.guesses[face] += addNumber;
+        return true;
+    }
+    makeBid(lastBid) {
+        // Simulates the NPC bid determination process 
+        let number = lastBid['number'], face = lastBid['face'];
+        let bidMin = face < this.seting.faces ? number : number + 1;
+
+        // Determine bid number
+        let bidNum = this._detBidNum(bidMin);
+        let bidFace = this._detBidFace(lastBid, bidNum);
+        this.placeBid(bidNum, bidFace);
+    }
+    _detBidNum(bidMin) {
+        // Simulates the NPC bid number determination process
+        do {
+            let bidNum = bidMin, multiplier = this.settings.bidNumMultiplier;
+            let condProb = binCondCumProb(totalDice, bidNum+1, bidMin, this.p);
+            let roll = Math.random()
+        } while (roll < condProb * multiplier) {
+            bidNum++;
+            condProb = binCondCumProb(totalDice, bidNum+1, bidMin, this.p);
+            roll = Math.random()
+        }
+        return bidNum;
+    }
+    _detBidFace(lastBid, bidNum) {
+        // Simulates the NPC bid face determination process
+        let faceMin = bidNum > lastBid['number'] ? 1 : lastBid['face'] + 1;
+        for (const [face, number] of Object.entries(this.guesses)) {
+            if (number > bidNum - 1 && face > faceMin - 1) {
+                return face;
+            }
+        }
+        let condProb;
+        let roll = Math.random(), maxProb = 0, maxFace = 0;
+        for (face = faceMin; face < this.settings.faces + 1; faces++) {
+            condProb = binCondCumProb(totalDice, bidNum, this.guesses[face], this.p);
+            if (condProb > roll) {
+                return face;
+            }
+            if (condProb > maxProb) {
+                maxProb = condProb;
+                maxFace = face;
+            }
+        }
+        return maxFace;
+    }   
+}
--- player.js
+++ player.js
@@ -3,24 +3,24 @@ class Player {
     totalRolled = {};
     constructor(game, name="Player") {
         // Initializes the player
-        this.active = true
+        this.active = true;
         this.settings = game.settings;
         this.name = name;
         this.numDice = this.settings.numDice;
         totalDice += this.numDice;
-        this.endTurn()
+        this.endTurn();
     }
     rollDice() {
-        // Rolls the players' dice
+        // Rolls the player's dice
         for (let die = 1; die < this.numDice + 1; die++){
             let face = Math.floor(Math.random() * this.settings.faces) + 1;
-            this.rolled[face] ? this.rolled[face]++ : this.rolled[face] = 1;
+            this.rolled[face]++;
             totalRolled[face] ? totalRolled[face]++ : totalRolled[face] = 1;
         }
     }
     placeBid(number, face) {
         // Records player's bid
-        this.bidNum, this.bidFace = number, face;
+        this.bidNum = number, this.bidFace = face;
     }
     loseDie() {
         /* Removes one of the player's dice. If player runs out of dice, 
@@ -34,6 +34,9 @@ class Player {
     endTurn() {
         // Turn clean up
         this.rolled = {}
+        for (let face = 1; face < this.settings.faces + 1; face++){
+            this.rolled[face] = 0;
+        }
         this.bidNum, this.bidFace = 0, 1;
     }
 }
