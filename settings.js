16530b332f2320c37094f7dd8ec4c2693ca96a9b
Victor <vfernandez.fw@gmail.com>
2024-11-16 18:26:51 -0600

    Updates

--- settings.js
+++ settings.js
@@ -7,22 +7,22 @@ export default class Settings {
 
         // Game Images
         this.dieImages = {
-            1: "./images/die/face_one.PNG",
-            2: "./images/die/face_two.PNG",
-            3: "./images/die/face_three.PNG",
-            4: "./images/die/face_four.PNG",
-            5: "./images/die/face_five.PNG",
-            6: "./images/die/face_six.PNG"
+            1: "./images/die/face_one.png",
+            2: "./images/die/face_two.png",
+            3: "./images/die/face_three.png",
+            4: "./images/die/face_four.png",
+            5: "./images/die/face_five.png",
+            6: "./images/die/face_six.png"
         };
         this.portraits = {
-            1: "./images/npc_x/one.PNG",
-            2: "./images/npc_x/two.PNG",
-            3: "./images/npc_x/three.PNG",
-            4: "./images/npc_x/four.PNG",
-            5: "./images/npc_x/five.PNG",
-            6: "./images/npc_x/six.PNG",
-            7: "./images/npc_x/seven.PNG",
-            8: "./images/npc_x/eight.PNG",
+            1: "./images/npc_x/one.png",
+            2: "./images/npc_x/two.png",
+            3: "./images/npc_x/three.png",
+            4: "./images/npc_x/four.png",
+            5: "./images/npc_x/five.png",
+            6: "./images/npc_x/six.png",
+            7: "./images/npc_x/seven.png",
+            8: "./images/npc_x/eight.png",
         }
         this.diceLeft = {
             1: "./images/npc_1/die.png",
